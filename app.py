eb8b93d7ef3113e57b53c5b38f897de7549f4294
Michiel Nafteux <Michiel.nafteux@student.ucll.be>
2025-12-15 16:04:44 +0100

    Code verwerkt tot key interaction

--- app.py
+++ app.py
@@ -1,13 +1,37 @@
 import pygame
 from pygame.display import flip
+from sys import exit
+
+class State:
+    def __init__(self):
+        self.x = 0
+
+    def increment(self, amount):
+        self.x += amount
+
+class Keyboard:
+    def __init__(self):
+        self.game_running = True
+        
+    def is_key_down(key):
+        while self.game_running:
+            if keyboard.is_key_down(RIGHT_ARROW):
+                player_x_position += 1
+      
+
+
 
 def create_main_surface():
     #SCHERM AANMAKEN 
     screen_size = (1024, 768)
     return pygame.display.set_mode(screen_size)
 
+clock = pygame.time.Clock()
+
 #main root function
 def main():
+    cord = State()
+
     #pygame init
     
     pygame.init()
@@ -21,12 +45,26 @@ def main():
                 exit()
 
         pygame.display.update()
-        render_frame(surface)
+
+        #geeft 60 fps
+        
+        cord.increment(1)
+        render_frame(surface, cord)
+
+        
+  
+
+        
        
 surface = create_main_surface()
 
-def render_frame(surface):
-        pygame.draw.circle(surface, (255,255,255), (400, 300), 50)
+def render_frame(surface, cord):
+        #render een frame
+        clear_surface(surface)
+        pygame.draw.circle(surface, (255,255,255), (cord.x, 300), 50)
         flip()
+
+def clear_surface(surface):
+    surface.fill((0, 0, 0))
 
 main()
