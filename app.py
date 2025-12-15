017b22a5474cad52710918fdb3e06bd2dbeb5df4
Lucas <Lucas.Claes@student.ucll.be>
2025-12-15 14:08:16 +0100

    init and loop

---
+++ app.py
@@ -0,0 +1,16 @@
+#main root function
+def main():
+    #pygame init
+    import pygame
+    pygame.init()
+
+    #SCHERM AANHOUDEN
+    while True:
+        None
+
+def create_main_surface():
+    #SCHERM AANMAKEN 
+    screen_size = (1024, 768)
+    pygame.display.set_mode(screen_size)
+
+
