6c9e266d9f8f9fd02a8fdf3f7bfe506bfd8b1e1d
Michiel Nafteux <Michiel.nafteux@student.ucll.be>
2025-12-15 14:55:15 +0100

    CIRCLEEEEE

--- app.py
+++ app.py
@@ -1,4 +1,10 @@
 import pygame
+from pygame.display import flip
+
+def create_main_surface():
+    #SCHERM AANMAKEN 
+    screen_size = (1024, 768)
+    return pygame.display.set_mode(screen_size)
 
 #main root function
 def main():
@@ -8,12 +14,19 @@ def main():
     create_main_surface()
     #SCHERM AANHOUDEN
     while True:
-        pass
+        #Alle render functies in while loop!
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                exit()
 
-def create_main_surface():
-    #SCHERM AANMAKEN 
-    screen_size = (1024, 768)
-    pygame.display.set_mode(screen_size)
+        pygame.display.update()
+        render_frame(surface)
+       
+surface = create_main_surface()
 
+def render_frame(surface):
+        pygame.draw.circle(surface, (255,255,255), (400, 300), 50)
+        flip()
 
 main()
