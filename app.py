9af3ed038158c9c5b1dbd208b5536cabc37e01b1
Lucas <Lucas.Claes@student.ucll.be>
2025-12-15 16:29:37 +0100

    movement

--- app.py
+++ app.py
@@ -2,25 +2,19 @@ import pygame
 from pygame.display import flip
 from sys import exit
 
+
+
 class State:
     def __init__(self):
         self.x = 0
+        self.y = 0
 
-    def increment(self, amount):
+    def incrementx(self, amount):
         self.x += amount
 
-class Keyboard:
-    def __init__(self):
-        self.game_running = True
-        
-    def is_key_down(key):
-        while self.game_running:
-            if keyboard.is_key_down(RIGHT_ARROW):
-                player_x_position += 1
+    def incrementy(self, amount):
+        self.y += amount
       
-
-
-
 def create_main_surface():
     #SCHERM AANMAKEN 
     screen_size = (1024, 768)
@@ -47,8 +41,16 @@ def main():
         pygame.display.update()
 
         #geeft 60 fps
-        
-        cord.increment(1)
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RIGHT:
+                cord.incrementx(1)
+            if event.key == pygame.K_LEFT:
+                cord.incrementx(-1)
+            if event.key == pygame.K_UP:
+                cord.incrementy(-1)
+            if event.key == pygame.K_DOWN:
+                cord.incrementy(1)
+
         render_frame(surface, cord)
 
         
@@ -61,7 +63,7 @@ surface = create_main_surface()
 def render_frame(surface, cord):
         #render een frame
         clear_surface(surface)
-        pygame.draw.circle(surface, (255,255,255), (cord.x, 300), 50)
+        pygame.draw.circle(surface, (255,255,255), (cord.x, cord.y), 50)
         flip()
 
 def clear_surface(surface):
